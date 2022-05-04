80388e1997e10abe640048d743feb00b836d512a
User <adj155731@gmail.com>
2022-05-04 17:22:48 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k=1,m;
+        for(int i = 1; i<=n; i++) {
+            k=k*i;
+
+        } System.out.println(n+"!"+"="+k);
+    }
+}
